993f6f7ff2828a51deb23604e59dfbdfc18a91b9
SrijayAnu <anusha.cherukuri16@gmail.com>
2025-05-03 06:48:29 +0530

    Create add.py

---
+++ add.py
@@ -0,0 +1,8 @@
+#### Addition Function ########
+
+def add_num(a,b):
+    sum=a+b;
+    return sum; 
+num1=int(input("input the number one: "))
+num2=int(input("input the number one :"))
+print("The sum is",add_num(num1,num2))
