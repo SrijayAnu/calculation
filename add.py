9ab9525e3aab61af862ab99c060944c1bfb6e187
SrijayAnu <anusha.cherukuri16@gmail.com>
2025-05-03 06:57:06 +0530

    adding floating number addition

--- add.py
+++ add.py
@@ -6,3 +6,12 @@ def add_num(a,b):
 num1=int(input("input the number one: "))
 num2=int(input("input the number one :"))
 print("The sum is",add_num(num1,num2))
+
+#############
+
+def add_num(a,b)
+    sum=a+b;
+    return sum; 
+num1=float(input("input the number one: "))
+num2=float(input("input the number one: "))
+print("The sum is",add_num(num1,num2))
